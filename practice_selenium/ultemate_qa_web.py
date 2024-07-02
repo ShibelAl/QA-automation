96d84b5fcfd1797d0f55036ea3af5eb119dfaec2
ShibelAl <shibelalshech300@gmail.com>
2024-07-03 01:00:44 +0300

    improved version

--- practice_selenium/ultemate_qa_web.py
+++ practice_selenium/ultemate_qa_web.py
@@ -12,31 +12,63 @@ driver = webdriver.Chrome()
 driver.get("https://www.google.com/")
 
 try:
-    # APjFqb is the id of the search bar
+    driver.implicitly_wait(10)
+    # go to "ultimate qa"
     search_input = WebDriverWait(driver, 2).until(ec.presence_of_element_located((By.ID, "APjFqb")))
     search_input.send_keys("ultimate qa")
+    time.sleep(1)
     search_input.send_keys(Keys.RETURN)
 
-    automation_practice_link = (WebDriverWait(driver, 2).until(
-        ec.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Automation Practice')]"))))
-    automation_practice_link.click()
+    # Go to Automation Practice
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//span[contains(text(), 'Automation Practice')]").click()
 
-    first_item_in_list = (WebDriverWait(driver, 2).until(
-        ec.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Big page with many elements')]"))))
-    first_item_in_list.click()
-    time.sleep(2)
+    # Go to Big page with many elements
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//a[contains(text(), 'Big page with many elements')]").click()
+
+    # Count how many green buttons there
     button_count = len(driver.find_elements(By.XPATH, "//a[text()='Button']"))
     print(button_count)
 
-    for i in range(1, button_count+1):
-        driver.find_element(By.XPATH, f"//a[@class='et_pb_button et_pb_button_{i-1} et_pb_bg_layout_light']").click()
+    # click on each green button
+    time.sleep(1)
+    for i in range(1, button_count + 1):
+        driver.find_element(By.XPATH, f"//a[@class='et_pb_button et_pb_button_{i - 1} et_pb_bg_layout_light']").click()
         time.sleep(1)
 
-    twitter_button = driver.find_element(By.XPATH, "//li[contains(@class,'et_pb_social_media_follow_network_0')]//a[@class='icon et_pb_with_border' and @title='Follow on Twitter']")
-    time.sleep(0.5)
-    twitter_button.click()
+    # Enter twitter
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//li[contains(@class,'et_pb_social_media_follow_network_0')]//a[@class='icon "
+                                  "et_pb_with_border' and @title='Follow on Twitter']").click()
+    time.sleep(8)
 
-    time.sleep(10)
+    # If more that one back() is needed
+    while driver.current_url != "https://ultimateqa.com/complicated-page":
+        driver.back()
+
+    # Enter the name
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[@id='et_pb_contact_name_0']").send_keys("Shibel")
+
+    # Enter the Email
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[@id='et_pb_contact_email_0']").send_keys("Shibel@gmail.com")
+
+    # Enter the Message
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//textarea[@id='et_pb_contact_message_0']").send_keys("Hello Selenium!")
+
+    # Enter the answer of the equation
+    time.sleep(1)
+    (driver.find_element(By.XPATH, "//input[@class='input et_pb_contact_captcha' and @name='et_pb_contact_captcha_0']")
+     .send_keys("5"))
+
+    # Press on the Submit button
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//div[@id='et_pb_contact_form_0']//button[@name='et_builder_submit_button']").click()
+
+    time.sleep(8)
     driver.quit()
 
 except Exception as e:
