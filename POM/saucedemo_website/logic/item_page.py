af22a6395c8129d93cc763362f1e4371da820790
ShibelAl <shibelalshech300@gmail.com>
2024-07-05 13:37:46 +0300

    added tests for the item page

--- POM/saucedemo_website/logic/item_page.py
+++ POM/saucedemo_website/logic/item_page.py
@@ -1,18 +1,25 @@
 from selenium.webdriver.common.by import By
-from base_page_app import BasePageApp
+from saucedemo_website.logic.base_page_app import BasePageApp
 
 
 class ItemPage(BasePageApp):
-    REMOVE_BUTTON = (By.XPATH, '//button[@id = "remove"]')
-    BACK_TO_PRODUCTS_BUTTON = (By.XPATH, '//button[@id = "back-to-products"]')
+    ADD_TO_CART_BUTTON = '//button[@id="add-to-cart"]'
+    REMOVE_BUTTON = '//button[@id = "remove"]'
+    BACK_TO_PRODUCTS_BUTTON = '//button[@id = "back-to-products"]'
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._remove_button = driver.find_element(self.REMOVE_BUTTON)
-        self._back_to_products_button = driver.find_element(self.BACK_TO_PRODUCTS_BUTTON)
+        self._add_to_cart_button = self._driver.find_element(By.XPATH, self.ADD_TO_CART_BUTTON)
+        self._remove_button = None
+        self._back_to_products_button = None
+
+    def click_on_add_to_cart_button(self):
+        self._add_to_cart_button.click()
 
     def click_on_remove_button(self):
+        self._remove_button = self._driver.find_element(By.XPATH, self.REMOVE_BUTTON)
         self._remove_button.click()
 
     def click_on_back_to_products_button(self):
+        self._back_to_products_button = self._driver.find_element(By.XPATH, self.BACK_TO_PRODUCTS_BUTTON)
         self._back_to_products_button.click()
--- POM/saucedemo_website/test/test_item_page.py
+++ POM/saucedemo_website/test/test_item_page.py
@@ -1,16 +1,80 @@
+import time
 import unittest
+from saucedemo_website.infra.config_provider import ConfigProvider
 from saucedemo_website.infra.browser_wrapper import BrowserWrapper
+from saucedemo_website.logic.login_page import LoginPage
+from saucedemo_website.logic.home_page import HomePage
 from saucedemo_website.logic.item_page import ItemPage
 
 
 class TestLoginPage(unittest.TestCase):
+    config = ConfigProvider.load_from_file('../config.json')
 
-    def test_remove_item_button(self):
-        driver = BrowserWrapper().getDriver("https://www.saucedemo.com/inventory-item.html?id=4")
+    def test_add_item_to_cart_in_item_page(self):
+        """
+        This function tests adding an item to cart from the item page
+        """
+        driver = BrowserWrapper().get_driver(self.config["base_url"])
+        login_page = LoginPage(driver)
+        login_page.login_flow("standard_user", "secret_sauce")
+        home_page = HomePage(driver)
+        home_page.click_on_backpack_item_title()
+        time.sleep(1)
         item_page = ItemPage(driver)
+        item_page.click_on_add_to_cart_button()
+        time.sleep(1)
+        driver.quit()
+
+    def test_add_and_remove_from_cart_flow(self):
+        """
+        This function tests adding an item to cart and then removing it.
+        and this happens in the item page.
+        """
+        driver = BrowserWrapper().get_driver(self.config["base_url"])
+        login_page = LoginPage(driver)
+        login_page.login_flow("standard_user", "secret_sauce")
+        home_page = HomePage(driver)
+        home_page.click_on_backpack_item_title()
+        time.sleep(1)
+        item_page = ItemPage(driver)
+        item_page.click_on_add_to_cart_button()
+        time.sleep(1)
         item_page.click_on_remove_button()
+        time.sleep(1)
+        driver.quit()
 
     def test_back_to_products_button(self):
-        driver = BrowserWrapper().getDriver("https://www.saucedemo.com/inventory-item.html?id=4")
+        """
+        This function tests back to products button in the item page
+        """
+        driver = BrowserWrapper().get_driver(self.config["base_url"])
+        login_page = LoginPage(driver)
+        login_page.login_flow("standard_user", "secret_sauce")
+        home_page = HomePage(driver)
+        home_page.click_on_backpack_item_title()
+        time.sleep(1)
         item_page = ItemPage(driver)
         item_page.click_on_back_to_products_button()
+        time.sleep(1)
+        driver.quit()
+
+    def test_adding_and_removing_item_flow(self):
+        """
+        This function tests the process of pressing on a product (item) to enter the
+        item page, and then adding the item to cart, removing it, and then pressing
+        on the back to products button.
+        """
+        driver = BrowserWrapper().get_driver(self.config["base_url"])
+        login_page = LoginPage(driver)
+        login_page.login_flow("standard_user", "secret_sauce")
+        home_page = HomePage(driver)
+        home_page.click_on_backpack_item_title()
+        time.sleep(1)
+        item_page = ItemPage(driver)
+        item_page.click_on_add_to_cart_button()
+        time.sleep(1)
+        item_page.click_on_remove_button()
+        time.sleep(1)
+        item_page.click_on_back_to_products_button()
+        time.sleep(1)
+        driver.quit()
