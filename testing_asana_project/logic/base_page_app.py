e87f32dfd6a96d6f17438758af3a3fee1abb4558
ShibelAl <shibelalshech300@gmail.com>
2024-07-11 14:36:28 +0300

    added tests for test_page_app

--- testing_asana_project/logic/base_page_app.py
+++ testing_asana_project/logic/base_page_app.py
@@ -7,11 +7,15 @@ from infra.base_page import BasePage
 class BasePageApp(BasePage):
     CREATE_BUTTON = '//div[@aria-label = "Create"]'
     PROJECT_BUTTON_IN_CREATE = '//div[@class = "MenuItemA11y Omnibutton-menuItem Omnibutton-project"]'
+    BLANK_PROJECT_BUTTON = '//div[@class = "DashedTile DashedTile--large FlowPickerTile-dashedTile"]'
+    POP_UP_AFTER_PRESSING_CREATE = '//div[contains(@class, "LayerPositioner-layer")]'
 
     def __init__(self, driver):
         super().__init__(driver)
         self._create_button = None
         self._project_button_in_create = None
+        self._blank_project_button = None
+        self._pop_up_after_pressing_create = None
 
     def click_on_create_button(self):
         self._create_button = WebDriverWait(self._driver, 10).until(
@@ -28,3 +32,13 @@ class BasePageApp(BasePage):
     def open_new_project(self):
         self.click_on_create_button()
         self.click_on_project_button_in_create()
+
+    def pop_up_after_pressing_create_is_displayed(self):
+        return WebDriverWait(self._driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.POP_UP_AFTER_PRESSING_CREATE))
+        ).is_displayed()
+
+    def blank_project_button_is_displayed(self):
+        return WebDriverWait(self._driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, self.BLANK_PROJECT_BUTTON))
+        ).is_displayed()
--- testing_asana_project/test/test_base_page_app.py
+++ testing_asana_project/test/test_base_page_app.py
@@ -11,9 +11,10 @@ class TestBasePageApp(unittest.TestCase):
     def setUp(self):
         self.browser = BrowserWrapper()
         self.config = ConfigProvider.load_config_json()
+        self.secret = ConfigProvider.load_secret_json()
         self.driver = self.browser.get_driver(self.config["base_url"])
         self.login_page = LoginPage(self.driver)
-        self.login_page.login_flow(self.config["email"], self.config["password"])
+        self.login_page.login_flow(self.config["email"], self.secret["password"])
 
     def tearDown(self):
         self.driver.quit()
@@ -21,8 +22,9 @@ class TestBasePageApp(unittest.TestCase):
     def test_create_button(self):
         base_page_app = BasePageApp(self.driver)
         base_page_app.click_on_create_button()
+        self.assertTrue(base_page_app.pop_up_after_pressing_create_is_displayed())
 
     def test_opening_new_project_window(self):
         base_page_app = BasePageApp(self.driver)
         base_page_app.open_new_project()
-        time.sleep(5)
+        self.assertEqual(self.driver.current_url, "https://app.asana.com/0/projects/new")
