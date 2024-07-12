fd8eb33038842977d543334e6c559ac6d04b4273
ShibelAl <shibelalshech300@gmail.com>
2024-07-12 11:40:52 +0300

    module for the sidebar (menu), for now, it is just to click on reporting page

---
+++ testing_asana_project/logic/sidebar.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from infra.base_page import BasePage
+
+
+class SideBar(BasePage):
+    REPORTING_BUTTON = '//a[@aria-label = "Reporting"]'
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self._reporting_button = WebDriverWait(self._driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.REPORTING_BUTTON))
+        )
+
+    def click_on_reporting_button(self):
+        self._reporting_button.click()
+    
