b9856a4febdf50821b18e9701406e4b404bf61d2
ShibelAl <shibelalshech300@gmail.com>
2024-07-12 18:34:59 +0300

    improved code efficiency and readability

--- testing_asana_project/logic/base_page_app.py
+++ testing_asana_project/logic/base_page_app.py
@@ -7,6 +7,7 @@ from infra.base_page import BasePage
 class BasePageApp(BasePage):
     CREATE_BUTTON = '//div[@aria-label = "Create"]'
     PROJECT_BUTTON_IN_CREATE = '//div[@class = "MenuItemA11y Omnibutton-menuItem Omnibutton-project"]'
+    MESSAGE_BUTTON_IN_CREATE = '//span[text() = "Message"]'
     BLANK_PROJECT_BUTTON = '//div[@class = "DashedTile DashedTile--large FlowPickerTile-dashedTile"]'
     POP_UP_AFTER_PRESSING_CREATE = '//div[contains(@class, "LayerPositioner-layer")]'
 
@@ -14,20 +15,24 @@ class BasePageApp(BasePage):
         super().__init__(driver)
         self._create_button = None
         self._project_button_in_create = None
+        self._message_button_in_create = None
         self._blank_project_button = None
         self._pop_up_after_pressing_create = None
 
     def click_on_create_button(self):
-        self._create_button = WebDriverWait(self._driver, 10).until(
+        WebDriverWait(self._driver, 10).until(
             EC.presence_of_element_located((By.XPATH, self.CREATE_BUTTON))
-        )
-        self._create_button.click()
+        ).click()
 
     def click_on_project_button_in_create(self):
-        self._project_button_in_create = WebDriverWait(self._driver, 10).until(
+        WebDriverWait(self._driver, 10).until(
             EC.presence_of_element_located((By.XPATH, self.PROJECT_BUTTON_IN_CREATE))
-        )
-        self._project_button_in_create.click()
+        ).click()
+
+    def click_on_message_button_in_create(self):
+        WebDriverWait(self._driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.MESSAGE_BUTTON_IN_CREATE))
+        ).click()
 
     def open_new_project(self):
         self.click_on_create_button()
