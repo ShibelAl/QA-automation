cd33382f8b535ff1c1692566de31c41f1e00c8e1
ShibelAl <shibelalshech300@gmail.com>
2024-07-15 15:13:13 +0300

    enum file that has the two location options for a chart in dashboard page, left and right

---
+++ testing_asana_project/logic/chart_location_options.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ChartLocationOptions(Enum):
+    """
+    Enum to represent the two options of chart location, one chart will
+    be in the left, and the other one will be in the right.
+    """
+    LEFT = 'left'
+    RIGHT = 'right'
