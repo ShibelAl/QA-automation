1e7a6e5e316d66d83c9b1fa3a713d4aee208a728
ShibelAl <shibelalshech300@gmail.com>
2024-07-20 00:59:09 +0300

    added post request function

--- blur_api_testing/infra/api/api_wrapper.py
+++ blur_api_testing/infra/api/api_wrapper.py
@@ -8,3 +8,7 @@ class APIWrapper:
     @staticmethod
     def get_request(url, header, querystring=None):
         return requests.get(url, headers=header, params=querystring)
+
+    @staticmethod
+    def post_request(url, header, payload=None):
+        return requests.post(url, headers=header, json=payload)
