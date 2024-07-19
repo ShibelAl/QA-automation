5667db797c531177e18b35bef18b53f5a57a2fb3
ShibelAl <shibelalshech300@gmail.com>
2024-07-19 20:34:36 +0300

    Bids - added 1 test - GET request

--- blur_api_testing/logic/api/bids.py
+++ blur_api_testing/logic/api/bids.py
@@ -4,8 +4,12 @@ from infra.config_provider import ConfigProvider
 class Bids:
     def __init__(self, request):
         self._request = request
+        self._config = ConfigProvider().load_config_json()
 
-    def get_executable_bids(self):
-        config = ConfigProvider().load_config_json()
-        url = f"{config['base_url']}/{config['executable_bids_endpoint']}"
-        return self._request.get_request(url, config['header'])
+    def get_retrieve_executable_bids(self):
+        url = f"{self._config['base_url']}/{self._config['retrieve_executable_bids_endpoint']}"
+        return self._request.get_request(url, self._config['header_1'])
+
+    def get_retrieve_user_bids(self):
+        url = f"{self._config['base_url']}/{self._config['retrieve_user_bids_endpoint']}"
+        return self._request.get_request(url, self._config['header_1'])
--- blur_api_testing/test/api/test_bids.py
+++ blur_api_testing/test/api/test_bids.py
@@ -16,8 +16,7 @@ class TestShuffleTheCards(unittest.TestCase):
         The querystring and the headers of the bid is in the config.json file
         """
         # Act
-        executable_bids_body = Bids(self._api_request).get_executable_bids().json()
-        print(executable_bids_body)  # print statement just for seeing the json body
+        executable_bids_body = Bids(self._api_request).get_retrieve_executable_bids().json()
         # Assert
         self.assertTrue(executable_bids_body['success'],
                         "The response did not succeed when retrieving executable bids.")
@@ -27,10 +26,22 @@ class TestShuffleTheCards(unittest.TestCase):
         Tests that executableSize is always greater than or equal to numberBidders
         for each price level in the response.
         """
-        executable_bids_body = Bids(self._api_request).get_executable_bids().json()
+        executable_bids_body = Bids(self._api_request).get_retrieve_executable_bids().json()
         for price_level in executable_bids_body['priceLevels']:
             executable_size = price_level['executableSize']
             number_bidders = price_level['numberBidders']
             self.assertGreaterEqual(executable_size, number_bidders,
                                     f"Executable size {executable_size} is not greater than or equal to number "
                                     f"of bidders {number_bidders} for price level {price_level['price']}")
+
+    def test_unauthorized_response_retrieve_user_bids(self):
+        """
+        Tests that if given an api request without an authentication key, then the
+        response will be a 401 status code, which mean unauthorized.
+        """
+        # Act
+        user_bids_body = Bids(self._api_request).get_retrieve_user_bids().json()
+        # Assert
+        self.assertEqual(user_bids_body['statusCode'], 401)
+
+
