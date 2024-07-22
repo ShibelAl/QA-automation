5a5a3c13d4568526dd2d1d6f614a781af0889c3d
ShibelAl <shibelalshech300@gmail.com>
2024-07-22 22:36:59 +0300

    added one test to verify that the user data is correct.

---
+++ linkedin_api_testing/test/test_profile_APIs.py
@@ -0,0 +1,42 @@
+import unittest
+from infra.api_wrapper import APIWrapper
+from infra.config_provider import ConfigProvider
+from logic.profile_APIs import ProfileAPIs
+
+
+class TestCompany(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set up the test environment by loading the configuration and initializing the API wrapper,
+        and then making a JobAPIs object, its API response and its json body.
+        """
+        self._config = ConfigProvider.load_config_json()
+        self._api_request = APIWrapper()
+        self.profile = ProfileAPIs(self._api_request)
+        self.response = self.profile.get_profile_data_by_url()
+
+    def test_if_user_data_is_correct(self):
+        """
+        Test if the user data is correct.
+
+        - Asserts that the response status code is 200.
+        - Asserts that the user data is equal to the expected data
+        :return:
+        """
+        # Act
+        response_body = self.response.json()
+        # Assert
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(response_body['username'], self._config['username'])
+        self.assertEqual(response_body['firstName'], self._config['firstName'])
+        self.assertEqual(response_body['lastName'], self._config['lastName'])
+        self.assertEqual(response_body['isCreator'], self._config['isCreator'])
+        self.assertEqual(response_body['isOpenToWork'], self._config['isOpenToWork'])
+        self.assertEqual(response_body['isHiring'], self._config['isHiring'])
+        self.assertEqual(response_body['profilePicture'], self._config['profilePicture'])
+        self.assertEqual(response_body['backgroundImage'], self._config['backgroundImage'])
+        self.assertEqual(response_body['summary'], self._config['summary'])
+        self.assertEqual(response_body['headline'], self._config['headline'])
+
+
