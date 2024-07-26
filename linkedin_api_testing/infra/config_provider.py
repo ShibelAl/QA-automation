0acd9b791ab6da5576c53eaf863076d7e1782590
ShibelAl <shibelalshech300@gmail.com>
2024-07-27 01:46:24 +0300

    added "load_secret_json" file, to load the secret file

--- linkedin_api_testing/infra/config_provider.py
+++ linkedin_api_testing/infra/config_provider.py
@@ -6,7 +6,17 @@ class ConfigProvider:
     @staticmethod
     def load_config_json():
         try:
-            with open("../config.json", 'r') as f:
+            with open(r"C:\IDE's\PyCharm\QA automation course\QA automation repo\QA-automation\linkedin_api_testing"
+                      r"\config.json", 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print(f"File not found.")
+
+    @staticmethod
+    def load_secret_json():
+        try:
+            with open(r"C:\IDE's\PyCharm\QA automation course\QA automation repo\QA-automation\linkedin_api_testing"
+                      r"\secret.json", 'r') as f:
                 return json.load(f)
         except FileNotFoundError:
             print(f"File not found.")
