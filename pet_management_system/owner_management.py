b0e54559b81b05b68fd00ea24183cf159590467c
ShibelAl <shibelalshech300@gmail.com>
2024-07-28 19:48:09 +0300

    file to handle all the functions related to a pet owner

---
+++ pet_management_system/owner_management.py
@@ -0,0 +1,44 @@
+import json
+from owner import Owner
+
+
+class OwnerManagement:
+    def __init__(self, filename="data.json"):
+        self.filename = filename
+        self.owners = []
+        self.load_owners()
+
+    def add_owner(self, owner):
+        self.owners.append(owner)
+        self.save_owners()
+
+    def add_pet_to_owner(self, pet, owner_phone_number):
+        for owner in self.owners:
+            if owner.phone_number == owner_phone_number:
+                owner.add_pet_to_owner_pets(pet)
+                break
+
+    def remove_pet_from_owner(self, pet, owner_phone_number):
+        for owner in self.owners:
+            if owner.phone_number == owner_phone_number:
+                owner.remove_pet_from_owner_pets(pet)
+                break
+
+    def load_owners(self):
+        """
+        Load library data from a JSON file into the library (books).
+        If the file does not exist, initialize an empty library.
+        """
+        try:
+            with open(self.filename, 'r') as file:
+                owner_dicts = json.load(file)
+                self.owners = [Owner(**owner_dict) for owner_dict in owner_dicts]
+        except FileNotFoundError:
+            self.owners = []
+
+    def list_owners(self):
+        return self.owners
+
+    def save_owners(self):
+        with open(self.filename, 'w') as file:
+            json.dump([owner for owner in self.owners], file)
