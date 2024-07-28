bbf458569aed1f00968ec6263e88b93900a619f8
ShibelAl <shibelalshech300@gmail.com>
2024-07-28 11:26:12 +0300

    added pet, owner, dog and cat classes

---
+++ pet_management_system/owner.py
@@ -0,0 +1,19 @@
+class Owner:
+    def __init__(self, name, phone_number):
+        self._name = name
+        self._phone_number = phone_number
+        self._pets = []
+
+    def add_pet_to_owner_pets(self, pet):
+        if pet.is_vaccinated:
+            self._pets.append(pet)
+
+    def remove_pet_to_owner_pets(self, pet):
+        if pet in self._pets:
+            self._pets.remove(pet)
+
+    def __str__(self):
+        pet_names = list(map(lambda pet: pet.name, self._pets))
+        return (f"Name: {self._name},"
+                f"Phone number: {self._phone_number}"
+                f"Pet's names': {pet_names}")
---
+++ pet_management_system/pet.py
@@ -0,0 +1,68 @@
+class Pet:
+    def __init__(self, name, species, age, owner, vaccinated):
+        self._name = None
+        self._species = None
+        self._age = None
+        self._owner = None
+        self._is_vaccinated = None
+
+        self.name = name
+        self.species = species
+        self.age = age
+        self.owner = owner
+        self.is_vaccinated = vaccinated
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        self._name = value
+
+    @property
+    def species(self):
+        return self._species
+
+    @species.setter
+    def species(self, value):
+        if not value:
+            raise ValueError("Species cannot be empty")
+        self._species = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise ValueError("Age must be a non-negative integer")
+        self._age = value
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, value):
+        self._owner = value
+
+    @property
+    def is_vaccinated(self):
+        return self._is_vaccinated
+
+    @is_vaccinated.setter
+    def is_vaccinated(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("Vaccinated should be a bool")
+        self._is_vaccinated = value
+
+    def __str__(self):
+        return (f"Pet(name: {self.name},"
+                f" species: {self.species},"
+                f" age: {self.age},"
+                f" owner: {self.owner},"
+                f" vaccinated: {self.is_vaccinated})")
