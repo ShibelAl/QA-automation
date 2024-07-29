e52365f76be8a9d9b06e49de582a61b2c1509633
ShibelAl <shibelalshech300@gmail.com>
2024-07-29 15:02:04 +0300

    added to_dict function, returns the owner object as a dictionary

--- pet_management_system/owner.py
+++ pet_management_system/owner.py
@@ -4,6 +4,14 @@ class Owner:
         self._phone_number = phone_number
         self._pets = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def phone_number(self):
+        return self._phone_number
+
     def add_pet_to_owner_pets(self, pet):
         if pet.is_vaccinated:
             self._pets.append(pet)
@@ -11,6 +19,13 @@ class Owner:
     def remove_pet_from_owner_pets(self, pet):
         if pet in self._pets:
             self._pets.remove(pet)
+
+    def to_dict(self):
+        return {
+            'name': self._name,
+            'phone_number': self._phone_number,
+            'pets': [pet.to_dict() for pet in self._pets]
+        }
 
     def __str__(self):
         pet_names = list(map(lambda pet: pet.name, self._pets))
